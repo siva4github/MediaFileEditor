53f9e7414b40da73b10ec463205940943262f2a2
Siva Alamalakala <siva.alamalakala@realpage.com>
2020-11-28 22:34:36 +0530

    code init

--- obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace MediaFileEditor {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MediaFileEditor;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MediaFileEditor;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
